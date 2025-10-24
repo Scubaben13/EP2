0bb9611aea8714d9e82eef0cb4ca9b26ff81482e
Scubaben13 <benjaming@al.insper.edu.br>
2025-10-24 17:46:30 -0300

    ex2.py

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,18 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicoes.append([linha, coluna + i])
     
     return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    if nome_navio not in frota:
+        frota[nome_navio] = [posicoes]
+    else:
+        frota[nome_navio].append(posicoes)
+    
+    return frota
+
+          
+
+
+
